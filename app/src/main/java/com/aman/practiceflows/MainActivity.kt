32f488ac1b706e9a5b6ddb539e3ccc6e68253fe0
aman nishad <javaman0512@gmail.com>
2025-05-11 17:11:20 +0530

    intro flows

--- app/src/main/java/com/aman/practiceflows/MainActivity.kt
+++ app/src/main/java/com/aman/practiceflows/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.aman.practiceflows
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +22,20 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        GlobalScope.launch {
+            val data = producer()
+            data.collect{
+                Log.d("Flows", it.toString())
+            }
+        }
+    }
+
+    fun producer() = flow<Int>{
+        val list = listOf(1,2,3,4,5,6,7,8,9,10)
+        list.forEach {
+            delay(1000)
+            emit(it)
         }
     }
 }
