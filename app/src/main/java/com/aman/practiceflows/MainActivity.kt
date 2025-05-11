52024d5a2ef38cabf50e5f58ad4fe835d5b42057
aman nishad <javaman0512@gmail.com>
2025-05-11 16:58:57 +0530

    intro channels

--- app/src/main/java/com/aman/practiceflows/MainActivity.kt
+++ app/src/main/java/com/aman/practiceflows/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.aman.practiceflows
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-
+    val channel = Channel<Int>()  // Hot observable (keeps producing no matter if any subscriber/consumer consuming it )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +21,22 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        producer()
+        consumer()
+    }
+
+    fun producer(){
+        CoroutineScope(Dispatchers.Main).launch {
+            channel.send(1)
+            channel.send(2)
+        }
+    }
+
+    fun consumer(){
+        CoroutineScope(Dispatchers.Main).launch {
+           Log.d("Channels INTRO", channel.receive().toString())
+            Log.d("Channels INTRO", channel.receive().toString())
         }
     }
 }
